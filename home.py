844577dcdef1cca65085a20ea3d1e173e47f1c0a
GOGOPOWERRANGERFYF <392138779@qq.com>
2019-05-12 00:41:50 +0800

    modify

--- home.py
+++ home.py
@@ -1,10 +1,18 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 home = Flask(__name__)
 
 @home.route('/')
 def homepage():
     return render_template('home.html')
+
+@home.route('/login',methods=['GET','POST'])
+def login():
+    return render_template('login.html')
+
+@home.route('/articles')
+def articles():
+    return render_template('article.html')
 
 if __name__ == '__main__':
     home.run(debug=True,host='0.0.0.0')
