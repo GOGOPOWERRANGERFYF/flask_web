9b22154e69109b2baaec3a2472a3bc5a55fc556c
GOGOPOWERRANGERFYF <392138779@qq.com>
2019-05-12 23:56:16 +0800

    new

---
+++ decorator.py
@@ -0,0 +1,32 @@
+def wrapper(func):
+    def inner(*args,**kwargs):
+        return func(*args,**kwargs):
+    return inner
+#1,无参数装饰器，执行到这一步，将下面的index函数当作参数传入wrapper函数
+#2.wrapper(index) 
+#3. 返回inner         
+#4.index=inner    这一步 是装饰器的精髓    
+@wrapper   
+def index(request):
+    pass
+
+
+#@route装饰器url绑定view function视图函数原理
+def route(url):
+    def inner(func,*args,**kwargs):
+        url_map['url'] = func
+        #return url
+    return inner
+#1.先执行route('/index')
+#2.返回inner       
+# 3.@inner
+# 4. index=inner(index)
+@route('/index')
+def index():
+    return 'index'
+
+
+url_map={
+    '/index':index
+}
+
