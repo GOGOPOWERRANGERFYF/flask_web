61343172322b3d20cced8996175adb42f25c60b3
GOGOPOWERRANGERFYF <392138779@qq.com>
2019-05-15 17:25:32 +0800

    modify

---
+++ importlib_example.py
@@ -0,0 +1,17 @@
+import importlib    #python提供importlib作为标准库的一部分      函数（function）类（class）> 模块moudle>包package>库library   个人理解，有待验证
+
+path = 'aaa.bbb'
+
+#print(path.split('.'))
+#print(path.rsplit('.'))
+
+a,b = path.split('.')   #python内置切片函数      path.split()   返回一个列表
+
+print(a)
+print(b)
+print(type(a))
+
+c,d = ['a',1]
+print(c)
+
+
